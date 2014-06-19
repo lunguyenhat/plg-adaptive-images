8ffaac071da768c91f789ef5b803b233af910a72
Lu Nguyen Nhat <lu@redweb.dk>
2014-06-19 14:44:36 +0700

    Adding basic files

---
+++ code/adaptive_images.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package     AdaptiveImages
+ * @subpackage  Plugin
+ *
+ * @copyright   Copyright (C) 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
